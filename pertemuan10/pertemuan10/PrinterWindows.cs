df3bdc7f0bbafcc9eb4872d7f432142de0ac7c37
hanafihp <fattaahherdansyah@students.amikom.ac.id>
2023-07-11 20:52:59 +0700

    first commit

---
+++ pertemuan10/pertemuan10/PrinterWindows.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace pertemuan10
+{
+	public class PrinterWindows
+	{
+		public string name { get; set; }
+		public string dimention { get; set; }
+
+        public PrinterWindows(string name, string dimention)  
+        {
+            this.name = name;
+            this.dimention = dimention;
+        }
+
+        public virtual void show()
+		{
+			Console.WriteLine("{0} display dimention : {1}", this.name, this.dimention);
+		}
+
+
+		public virtual void print()
+		{
+			Console.WriteLine("{0} printer printing....", this.name);
+
+        }
+	}
+
+
+	public class Epson : PrinterWindows
+	{
+        public Epson(string name, string dimention) : base(name, dimention)
+        {
+            this.name = name;
+            this.dimention = dimention;
+        }
+
+        public override void show()
+        {
+            Console.WriteLine("{0} display dimention : {1}", this.name, this.dimention);
+        }
+
+
+        public override void print()
+        {
+            Console.WriteLine("{0} printer printing....", this.name);
+
+        }
+ 
+    }
+
+
+    public class Canon : PrinterWindows
+    {
+        public Canon(string name, string dimention) : base(name, dimention)
+        {
+            this.name = name;
+            this.dimention = dimention;
+        }
+
+        public override void show()
+        {
+            Console.WriteLine("{0} display dimention : {1}", this.name, this.dimention);
+        }
+
+
+        public override void print()
+        {
+            Console.WriteLine("{0} printer printing....", this.name);
+
+        }
+
+    }
+
+
+
+    public class Lasetjet : PrinterWindows
+    {
+        public Lasetjet(string name, string dimention) : base(name, dimention)
+        {
+            this.name = name;
+            this.dimention = dimention;
+        }
+
+        public override void show()
+        {
+            Console.WriteLine("{0} display dimention : {1}", this.name, this.dimention);
+        }
+
+
+        public override void print()
+        {
+            Console.WriteLine("{0} printer printing....", this.name);
+
+        }
+
+    }
+}
+
---
+++ pertemuan10/pertemuan10/Program.cs
@@ -0,0 +1,41 @@
+﻿using pertemuan10;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Pilih Printer");
+        Console.WriteLine("================");
+        Console.WriteLine("1. Epson");
+        Console.WriteLine("2. Canon");
+        Console.WriteLine("3. LaserJet");
+
+        Console.Write("Nomor Printer [1..3] : ");
+        int nomorPrinter = Convert.ToInt32(Console.ReadLine());
+
+        PrinterWindows printerWindows = new PrinterWindows("cbdcbdj", "1100");
+        if (nomorPrinter == 1)
+        {
+            printerWindows = new Epson("Epson", "11*12");
+            printerWindows.show();
+            printerWindows.print();
+
+        }
+
+        else if (nomorPrinter == 2)
+        {
+            printerWindows = new Canon("Canon", "10*11");
+            printerWindows.show();
+            printerWindows.print();
+        }
+
+        else if (nomorPrinter == 3)
+        {
+            printerWindows = new Canon("LaserJet", "9*10");
+            printerWindows.show();
+            printerWindows.print();
+        }
+
+        Console.ReadLine();
+    }
+}
